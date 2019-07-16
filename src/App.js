d7b0c022fb0571d78cc3b3b805b69c5f06315c05
noahfranco <noahfra@gmail.com>
2019-07-16 18:46:23 -0400

    finished the numbers and operators

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators.js";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -12,12 +15,24 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+// const config = {}; 
+// const math = create(all, config)
+// const [calculation, setCalculation] = useState(0);
+
+// const update = value => {
+//   value === "x/-" ? setCalculation(math.eval(-calculation)) : value === '=' ? setCalculation(math.eval(calculation)) :
+//   value === "C" ? setCalculation(0) : calculation === 0 ? setCalculation(value) : setCalculation(calculation + value)
+// };
 
   return (
     <div className="container">
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        {/* < Display calculation={calculation} /> */}
+        <Numbers /> 
+        <Operators /> 
+        
       </div>
     </div>
   );
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
-
+import OperatorButton from "./OperatorButton.js"
 //Import your array data to from the provided data file
-
+import {operators} from "../../../data.js"
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operator, setOperator] = useState(operators)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operator.map((awesome) => {
+         return <OperatorButton awesome={awesome} />
+       })}
     </div>
   );
 };
+
+export default Operators; 
