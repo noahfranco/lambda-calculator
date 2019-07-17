3be0324e5a8b3b151ff41d906b3a85e6ea3343b6
noahfranco <noahfra@gmail.com>
2019-07-17 09:26:23 -0400

    working on styling

--- src/App.js
+++ src/App.js
@@ -10,20 +10,7 @@ import Logo from "./components/DisplayComponents/Logo";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators.js";
 
 function App() {
-  // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
-  // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
-  // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
-  // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
-  // Don't forget to pass the functions (and any additional data needed) to the components as props
-// const config = {}; 
-// const math = create(all, config)
-// const [calculation, setCalculation] = useState(0);
-
-// const update = value => {
-//   value === "x/-" ? setCalculation(math.eval(-calculation)) : value === '=' ? setCalculation(math.eval(calculation)) :
-//   value === "C" ? setCalculation(0) : calculation === 0 ? setCalculation(value) : setCalculation(calculation + value)
-// };
-
+  
   return (
     <div className="container">
       <Logo />
